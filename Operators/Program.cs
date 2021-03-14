658b0973242d611452e478e4cd7786a470b27edb
hamdiituu <62481358+hamdiituu@users.noreply.github.com>
2021-03-14 21:38:10 +0300

    operators

---
+++ Operators/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Operators
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Operators");
+            /*
+             * 
+             *  +
+             *  -
+             *  *
+             *  /
+             *  %
+             *  ++
+             *  --
+             */
+
+            /*
+             * ==
+             * !=
+             * >=
+             * >
+             * 
+             * &&
+             * ||
+             * !
+             * 
+             */
+
+            int x = 123 + 34;
+            Console.WriteLine("X is : " + x);
+
+            string firstName = "Hamdi";
+            string lastName = "Tug";
+
+            string user = firstName + " " + lastName;
+            Console.WriteLine(user);
+
+            int number = 101;
+            number++;
+            ++number;
+            Console.WriteLine("Number is :" + number);
+
+
+
+
+        }
+    }
+}
